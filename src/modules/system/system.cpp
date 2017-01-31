356dc3a4c9329576a64636c406952d09d5cee8da
Markus Lippert <lippertmarkus@gmx.de>
2017-01-31 18:56:42 +0100

    update system topic subscription to read function from message instead of topic

--- src/modules/system/system.cpp
+++ src/modules/system/system.cpp
@@ -1,27 +1,19 @@
 #include "system.h"
 
 void system_subscribe(PubSubClient mqtt_client) {
-    String topic = WiFi.hostname() + "/#";
+    String topic = WiFi.hostname();
     Serial.println("Subscribing to system topic " + topic);
     mqtt_client.subscribe(topic.c_str());
 }
 
 void system_mqtt_callback(String intopic, String message) {
-    char *topic = (char*)intopic.c_str();
-    char *base = strtok(topic, "/");
+    String topic = WiFi.hostname();
 
-    String wifi_host = WiFi.hostname();
-    const char *hostname = wifi_host.c_str();
-
-    if(strcmp(base, hostname) == 0) {
-        char *function = strtok(NULL, "/");
-
-        if (strcmp(function, "update") == 0) {
+    if(intopic.equals(topic)) {
+        if(message.equals("update")) {
             Serial.println("Updating firmware..");
             updateFirmware();
-        }
-
-        if (strcmp(function, "reboot") == 0) {
+        } else if(message.equals("reboot")) {
             Serial.println("Rebooting in 3 seconds..");
             delay(3000);
             ESP.restart();
