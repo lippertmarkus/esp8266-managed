8d14d373e0b182af8a98bf40b2c71ac95637214d
Markus Lippert <lippertmarkus@gmx.de>
2017-04-08 13:14:43 +0200

    use pins of real prototype as default

--- src/config.cpp
+++ src/config.cpp
@@ -20,19 +20,19 @@ const uint16_t mqtt_port = 1883;
 /*
  * Infrared
  */
-const unsigned int ir_led_pin = 13;
+const unsigned int ir_led_pin = 12;
 const String ir_topic = "foo/bar/ir/#";
 
 /*
  * 433MHz Transmitter
  */
-const unsigned int tx433mhz_pin = 12;
+const unsigned int tx433mhz_pin = 14;
 const String ws_topic = "foo/bar/wireless_socket/#";
 
 /*
  * DHT11
  */
-const int dht11_pin = 11;
+const int dht11_pin = 13;
 const unsigned long dht11_publish_cycle = 1800000; // every half hour
 const char* dht11_temperature_topic = "foo/bar/temperature";
 const char* dht11_humidity_topic = "foo/bar/humidity";
@@ -40,5 +40,5 @@ const char* dht11_humidity_topic = "foo/bar/humidity";
 /*
  * Contacts
  */
-const int contacts_pins[10] = {4}; // don't use pin 0
-const char* contacts_topics[10] = {"foo/bar/door1"};
+const int contacts_pins[10] = {5,4,16};  // don't use pin 0
+const char* contacts_topics[10] = {"foo/bar/door","foo/bar/window","foo/bar/motion"};
